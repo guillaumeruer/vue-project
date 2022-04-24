1e25ca82504700b293dca3df7e842cfd9fac3c33
guillaumeruer <guillaume.ruergmail.com>
2022-04-24 18:43:02 +0200

    add ignore

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import router from "./router";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAD1Xhi6T_1jSQ3z0F6YMqbnOZrscYRwUo",
-  authDomain: "vue-firebase-997eb.firebaseapp.com",
-  projectId: "vue-firebase-997eb",
-  storageBucket: "vue-firebase-997eb.appspot.com",
-  messagingSenderId: "377398502521",
-  appId: "1:377398502521:web:38b241d78a5ad8d96a20a8",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 initializeApp(firebaseConfig);
